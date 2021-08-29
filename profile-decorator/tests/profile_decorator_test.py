dfc0d28f5bdfa375397a3e3a0c7827440f7ffcdc
Harrison Kiang <hkiang01@gmail.com>
2021-08-29 11:59:26 -0500

    ci tests

--- profile-decorator/tests/profile_decorator_test.py
+++ profile-decorator/tests/profile_decorator_test.py
@@ -1,5 +1,8 @@
-from profile_decorator import __version__
+from profile_decorator import __version__, profile_decorator
+
+profile_decorator.init()
 
 
+@profile_decorator.profile_memory
 def test_version():
     assert __version__ == "0.1.0"
